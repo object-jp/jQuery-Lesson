b76586552ce3327c3fb8f9eb12a3ec4eef479388
object <object@seinoutaiganoMacBook-Pro.local>
2021-11-09 17:47:28 +0900

    learned this and children

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 $(function(){
-  $('.box1').mouseover(function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
-  });
-  $('.box1').on('click', function(){
-    $('.box1').addClass('box1-click');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-click');
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
